bac9b7f0c78fc39ab68438b047e69dcab9c2fe19
hf0220 <846303015@qq.com>
2018-03-28 08:39:46 +0800

    更新bbs缓存,新增MBG

--- bbs/src/main/java/com/hjl/mapper/MovieMapper.java
+++ bbs/src/main/java/com/hjl/mapper/MovieMapper.java
@@ -2,11 +2,14 @@ package com.hjl.mapper;
 
 import com.hjl.entity.Movie;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 import java.util.Map;
 
 public interface MovieMapper {
+
 
     List<Movie> findList(@Param("title") String title);
     List<Movie> findByParams(Map<String,Object> params);
--- bbs/src/main/java/com/hjl/mapper/StudentMapper.java
+++ bbs/src/main/java/com/hjl/mapper/StudentMapper.java
@@ -1,11 +1,14 @@
 package com.hjl.mapper;
 
 import com.hjl.entity.Student;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface StudentMapper {
 
+    @Select("select * from t_accoumt where id = #{id}")
     Student findById(int id);
     Student findByIdWithTag(int id);
     List<Student> findByAll();
